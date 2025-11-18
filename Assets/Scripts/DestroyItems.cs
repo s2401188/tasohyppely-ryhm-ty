eccb48c998d223a6fc3922634fe9d29d370a05c4
Jauhelihamies <s2401274@edu.bc.fi>
2025-11-18 10:14:40 +0200

    DestroyItemsScript

---
+++ Assets/Scripts/DestroyItems.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class DestroyItems : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+}
