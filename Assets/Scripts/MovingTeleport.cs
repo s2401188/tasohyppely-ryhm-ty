3b837734d2a311f9431f628362847b0ffc360342
Jauhelihamies <s2401274@edu.bc.fi>
2025-11-24 12:44:21 +0200

    You Cheated update

---
+++ Assets/Scripts/MovingTeleport.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MovingTeleport : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
