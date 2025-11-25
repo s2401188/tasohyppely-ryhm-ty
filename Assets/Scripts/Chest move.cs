eadf5612ccb7cadb468ff31a24bc8ff4f8b1cac6
Jauhelihamies <s2401274@edu.bc.fi>
2025-11-25 12:58:32 +0200

    Deathscreen

--- Assets/Scripts/Chest move.cs
+++ Assets/Scripts/Chest move.cs
@@ -3,23 +3,25 @@ using UnityEngine;
 public class ChestInfinityMotion : MonoBehaviour
 {
     public float speed = 1f;
-    public float size = 1f;
-    public float shineSpeed = 2f;
-    SpriteRenderer sr;
-    float t;
+    public float width = 1f;
+    public float height = 0.5f;
+
+    private Vector3 startPos;
+    private float t;
 
     void Start()
     {
-        sr = GetComponent<SpriteRenderer>();
+        startPos = transform.localPosition;
+        t = 0f;
     }
 
     void Update()
     {
         t += Time.deltaTime * speed;
-        float x = Mathf.Sin(t) * size;
-        float y = Mathf.Sin(t * 2f) * size * 0.5f;
-        transform.localPosition += new Vector3(x, y, 0) * Time.deltaTime;
-        float a = (Mathf.Sin(Time.time * shineSpeed) + 1f) * 0.4f + 0.2f;
-        sr.color = new Color(1f, 1f, 1f, a);
+
+        float x = Mathf.Sin(t) * width;
+        float y = Mathf.Sin(2f * t) * height;
+
+        transform.localPosition = startPos + new Vector3(x, y, 0);
     }
 }
--- Assets/Scripts/Coinspin.cs
+++ Assets/Scripts/Coinspin.cs
@@ -2,19 +2,22 @@ using UnityEngine;
 
 public class CoinSpinShine : MonoBehaviour
 {
-    public float spinSpeed = 180f;
-    public float shineSpeed = 2f;
+    public float bobSpeed = 2f;
+    public float bobHeight = 0.5f;
+    
     SpriteRenderer sr;
+    Vector3 startPos;
 
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        startPos = transform.localPosition;
     }
 
     void Update()
     {
-        transform.Rotate(0, 0, spinSpeed * Time.deltaTime);
-        float a = (Mathf.Sin(Time.time * shineSpeed) + 1f) * 0.4f + 0.2f;
-        sr.color = new Color(1f, 1f, 1f, a);
+        float yOffset = Mathf.Sin(Time.time * bobSpeed) * bobHeight;
+        transform.localPosition = startPos + new Vector3(0, yOffset, 0);
+
     }
 }
