40f3a86d0db48167f5ad7875c2477da60da2af9c
Jauhelihamies <s2401274@edu.bc.fi>
2025-11-26 11:48:16 +0200

    Level ∞ & easy mode options

---
+++ Assets/Scripts/GameMode.cs
@@ -0,0 +1,25 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLevel : MonoBehaviour
+{
+    public int Hard = 1;
+    public int Easy = 0;
+    public int Infinite = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void StartHard()
+    {
+        SceneManager.LoadScene(Hard);
+    }
+    public void StartEasy()
+    {
+        SceneManager.LoadScene(Easy);
+    }
+    public void StartInfinite()
+    {
+        SceneManager.LoadScene(Infinite);
+    }
+}
