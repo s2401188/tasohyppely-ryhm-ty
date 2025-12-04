1cccf11d0f792644601270de465b565d718ef6ee
Jauhelihamies <s2401274@edu.bc.fi>
2025-12-04 09:36:41 +0200

    New Code for level teleporting

--- Assets/Scripts/MovingTeleport.cs
+++ Assets/Scripts/MovingTeleport.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MovingTeleport : MonoBehaviour
+
 {
+    public int Location = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(Location);
         }
     }
 }
